5040d6b3fd19b09b11479c1c336f84aa13184510
AlexandrBxbrxv <g1026@bk.ru>
2024-09-11 17:50:55 +0500

    + модель MailingMessage

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from mailings.models import Client
+from mailings.models import Client, MailingMessage
 
 
 @admin.register(Client)
-class ServiceClientAdmin(admin.ModelAdmin):
+class ClientAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'full_name',)
     search_fields = ('full_name',)
+
+
+@admin.register(MailingMessage)
+class MailingMessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'msg_topic',)
+    search_fields = ('msg_topic',)
--- mailings/models.py
+++ mailings/models.py
@@ -4,11 +4,21 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class Client(models.Model):
-    email = models.EmailField(verbose_name='email', help_text='обязательное поле')
-    full_name = models.CharField(max_length=200, verbose_name='Ф.И.О.', help_text='обязательное поле')
+    email = models.EmailField(verbose_name='email')
+    full_name = models.CharField(max_length=200, verbose_name='Ф.И.О.')
     comment = models.TextField(**NULLABLE, verbose_name='комментарий')
 
     class Meta:
-        verbose_name = 'клиент сервиса'
-        verbose_name_plural = 'клиенты сервиса'
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
         ordering = ('full_name',)
+
+
+class MailingMessage(models.Model):
+    msg_topic = models.CharField(max_length=100, verbose_name='тема')
+    msg_body = models.TextField(verbose_name='содержимое')
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения для рассылок'
+        ordering = ('msg_topic',)
