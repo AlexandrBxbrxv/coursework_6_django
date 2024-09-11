e249107ee111efcefd9d64eb016f93a9fd2a391b
AlexandrBxbrxv <g1026@bk.ru>
2024-09-11 15:50:41 +0500

    + контроллёр добавления клиента

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from mailings.apps import MailingsConfig
 
-from mailings.views import HomeTemplateView, ClientListView, ClientDetailView
+from mailings.views import HomeTemplateView, ClientListView, ClientDetailView, ClientCreateView
 
 app_name = MailingsConfig.name
 
 urlpatterns = [
     path('', HomeTemplateView.as_view(), name='home'),
+    path('client/create', ClientCreateView.as_view(), name='client_create'),
     path('clients', ClientListView.as_view(), name='clients'),
     path('client<int:pk>/detail', ClientDetailView.as_view(), name='client_detail'),
 ]
--- mailings/views.py
+++ mailings/views.py
@@ -1,10 +1,17 @@
-from django.views.generic import TemplateView, ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 
 from mailings.models import Client
 
 
 class HomeTemplateView(TemplateView):
     template_name = 'mailings/home.html'
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ('email', 'full_name', 'comment',)
+    success_url = reverse_lazy('mailings:clients')
 
 
 class ClientListView(ListView):
